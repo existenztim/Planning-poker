8bd2b2e5baf4d58498c1e3b9a628de6470dfe132
MariaLBovin <maria.bovin@gmail.com>
2023-05-04 09:56:26 +0200

    socket added

---
+++ frontend/src/socket.ts
@@ -0,0 +1,8 @@
+import { io } from 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js';
+
+const URL = "http://localhost:3000";
+export const socket = io(URL);
+
+// socket.onAny((event, ...args) => {
+//   console.log(event, args);
+// });
