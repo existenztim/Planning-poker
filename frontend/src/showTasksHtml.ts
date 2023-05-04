dc62c10affa7a599bf51adec8c662a51d7329889
MariaLBovin <maria.bovin@gmail.com>
2023-05-04 15:05:49 +0200

    div-wrapper added to votingpage

--- frontend/src/showTasksHtml.ts
+++ frontend/src/showTasksHtml.ts
@@ -5,17 +5,20 @@ export default function showTaskHtml() {
   console.log(container);
   container.innerHTML = '';
   
+  const votingPageContainer : HTMLDivElement = document.createElement('div');
+  votingPageContainer.classList.add('votingpage');
   const todoTaskList: HTMLTableElement = document.createElement('table');
   todoTaskList.classList.add('todo-list');
+  todoTaskList.innerText = 'röstningslista'
   const votingContainer: HTMLDivElement = document.createElement('div');
   votingContainer.classList.add('voting-div');
-  votingContainer.innerText = 'allt som rör röstning';
   const doneTasksList: HTMLTableElement = document.createElement('table');
   doneTasksList.classList.add('done-tasks');
   doneTasksList.innerText='alla färdiga röstningar';
   
   //console.log(todoTaskList, votingContainer, doneTasksList);
-  container.append(todoTaskList, votingContainer, doneTasksList);
+  container.appendChild(votingPageContainer);
+  votingPageContainer.append(todoTaskList, votingContainer, doneTasksList);
   
   const displayVoteTask: HTMLDivElement = document.createElement('div');
   displayVoteTask.classList.add('voting-header');
