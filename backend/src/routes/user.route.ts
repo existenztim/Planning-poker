e60fa4826202cff772116bdb3b2e8074b15c3fc8
Therese <therese.holen22@gmail.com>
2023-05-04 15:04:23 +0200

    Feat. Added post to add new user in userrouter

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -2,6 +2,23 @@ import { Router } from 'express';
 
 const userRouter = Router();
 
-// userRouter.post('/add', addUser);
+import User from '../models/UserModel';
+import CryptoJS from "crypto-js";
+import { error } from 'console';
+
+
+userRouter.post('/add', async (req, res) => {
+  try {
+    const user = await User.create(req.body)
+    const passwordToSave = CryptoJS.SHA3(req.body.password).toString()
+    user.password = passwordToSave;
+    await user.save()
+    res.status(201).json({username:user.username, _id:user._id, admin:user.admin})
+  } catch (error){
+    console.log(error)
+    res.status(400).end()
+  }
+});
+//userRouter.post('/add', addUser);
 
 export default userRouter;
