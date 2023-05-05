f7644c8406dd41a433a72a8136f43c2dd86216d2
MariaLBovin <maria.bovin@gmail.com>
2023-05-05 10:26:02 +0200

    eventlistener and id added to each tr

--- frontend/src/getTasks.ts
+++ frontend/src/getTasks.ts
@@ -6,16 +6,26 @@ export default function getTasks() {
 
   socket.on('getList', (list) => {
     console.log(list);
+    let count = 0
     list.map(item => {
       console.log(item.title);
       const tr = document.createElement('tr');
+      tr.id = `tr-${count}`;
+      count++;
+
       const titleTd = document.createElement('td');
       titleTd.innerText = item.title;
       const descriptionTd = document.createElement('td');
       descriptionTd.innerText = item.description;
+      
 
       table.append(tr);
       tr.append(titleTd, descriptionTd);
+
+      tr.addEventListener('click', (e) => {
+        //console.log(e.currentTarget);
+        
+      })
     });
   });
 }
