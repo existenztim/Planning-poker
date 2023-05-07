dd5424960fa0e24b2f01ea16a662bef77e7a48cc
dough <viktor.melin@me.com>
2023-05-07 15:00:42 +0200

    use fetch instead of socket on initial connect

--- frontend/src/modules/sessionVue.ts
+++ frontend/src/modules/sessionVue.ts
@@ -1,133 +1,133 @@
 /* eslint-disable no-console */
-import { socket } from "../socket.ts";
-import renderTempAdminPage from "./adminVue.ts";
-import type { Task } from "../models/taskModel.ts";
-import type { User } from "../models/userModel.ts";
+import { socket } from '../socket.ts';
+import renderTempAdminPage from './adminVue.ts';
+import type { Task } from '../models/taskModel.ts';
+import type { User } from '../models/userModel.ts';
 
-export default function sessionVue () {
-  const printAppHtml =() => {
+export default function sessionVue() {
+  const printAppHtml = () => {
     const container: HTMLElement = document.querySelector('#app') as HTMLElement;
     // eslint-disable-next-line no-console
     container.innerHTML = '';
-    
-    const votingPageContainer : HTMLDivElement = document.createElement('div');
+
+    const votingPageContainer: HTMLDivElement = document.createElement('div');
     votingPageContainer.classList.add('votingpage');
     const todoTaskList: HTMLTableElement = document.createElement('table');
     todoTaskList.classList.add('todo-list');
-    todoTaskList.innerText = 'röstningslista'
+    todoTaskList.innerText = 'röstningslista';
     const votingContainer: HTMLDivElement = document.createElement('div');
     votingContainer.classList.add('voting-div');
     const doneTasksList: HTMLTableElement = document.createElement('table');
     doneTasksList.classList.add('done-tasks');
-    doneTasksList.innerText='alla färdiga röstningar';
-    
+    doneTasksList.innerText = 'alla färdiga röstningar';
+
     //console.log(todoTaskList, votingContainer, doneTasksList);
     container.appendChild(votingPageContainer);
     votingPageContainer.append(todoTaskList, votingContainer, doneTasksList);
-    
+
     const displayVoteTask: HTMLDivElement = document.createElement('div');
     displayVoteTask.classList.add('voting-header');
     displayVoteTask.innerText = 'vad vi ska rösta på';
-    
+
     const voteCardsContainer: HTMLDivElement = document.createElement('div');
     voteCardsContainer.classList.add('voting-card-container');
-    voteCardsContainer.innerText= 'våra röstkost';
-  
-    
+    voteCardsContainer.innerText = 'våra röstkost';
+
     votingContainer.append(displayVoteTask, voteCardsContainer);
     printHeaderHtml();
-  }
+  };
 
-  const createUserCards = () => {
-    socket.on('userList', (userList:User[]) => {
-      console.log(userList);
-      const votingCardContainer : HTMLDivElement = document.querySelector('.voting-card-container') as HTMLDivElement;
+  const createUserCards = async () => {
+    const response = await fetch(`http://localhost:5050/api/vote/sessions`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.status === 200) {
+      const users = (await response.json()) as User[];
+      console.log('Users in session', users);
+
+      const votingCardContainer: HTMLDivElement = document.querySelector('.voting-card-container') as HTMLDivElement;
       votingCardContainer.innerHTML = '';
-  
-      userList.map(user => {
-        console.log(user);
-        const votingCard : HTMLDivElement = document.createElement('div');
+
+      users.map((user) => {
+        const votingCard: HTMLDivElement = document.createElement('div');
         votingCard.classList.add('voting-card-div');
         votingCard.innerText = 'Röstkort';
-        votingCard.innerHTML = /*html */`<p>${user.username}s: poäng</p>
+
+        // if (user.username)
+
+        votingCard.innerHTML = /*html */ `<p>${user.username}s: poäng</p>
         <select name="points" id="points">
           <option value=null>Välj</option>
           <option value=1>Tiny 1SP</option>
           <option value=3>Small 3SP</option>
           <option value=5>Medium 5SP</option>
           <option value=8>Large 8 SP</option>
-        </select>`
-  
-        votingCardContainer.append(votingCard)
-          
-          
-      })
-      
-    })
-      
-  }
+        </select>`;
 
-  const printHeaderHtml =() => {
+        votingCardContainer.append(votingCard);
+      });
+    }
+  };
+
+  const printHeaderHtml = () => {
     const admin = true; // Tillfällig lösning
     const headerContainer = document.querySelector('#header') as HTMLHeadingElement;
-    const headerTag = /*html*/`<h1>Planning Poker</h1>`;
+    const headerTag = /*html*/ `<h1>Planning Poker</h1>`;
     let adminButton = '';
-  
-    if(admin) { //kontrollera med localstorage (user.admin någon sådant)
+
+    if (admin) {
+      //kontrollera med localstorage (user.admin någon sådant)
       adminButton = `<button id='adminMode'>Admin Läge</button>`;
     }
-  
-    headerContainer.innerHTML =/*html */`
+
+    headerContainer.innerHTML = /*html */ `
       ${headerTag}
       <div class="button-container">
         ${adminButton}
         <button id='logOut'>Logga ut</button>
       </div>
-    `
+    `;
     headerBtnsEvent();
-  }
-  
+  };
+
   const headerBtnsEvent = () => {
     const adminBtn = document.getElementById('adminMode');
     adminBtn?.addEventListener('click', () => {
       renderTempAdminPage();
-    })
-  }
+    });
+  };
 
-  const getTasks =() => {
+  const getTasks = () => {
     const table: HTMLTableElement = document.querySelector('.todo-list') as HTMLTableElement;
-  
-    socket.on('getList', (list:Task[]) => {
+
+    socket.on('getList', (list: Task[]) => {
       console.log(list);
-      let count = 0
-      list.map(item => {
+      let count = 0;
+      list.map((item) => {
         console.log(item.title);
         const tr = document.createElement('tr');
         tr.id = `tr-${count}`;
         count++;
-  
+
         const titleTd = document.createElement('td');
         titleTd.innerText = item.title;
         const descriptionTd = document.createElement('td');
         descriptionTd.innerText = item.description;
-        
-  
+
         table.append(tr);
         tr.append(titleTd, descriptionTd);
-  
+
         tr.addEventListener('click', (e) => {
           //console.log(e.currentTarget);
-          
-        })
+        });
       });
     });
-  }
+  };
   printAppHtml();
   createUserCards();
   getTasks();
 }
-
-
-
-
-
