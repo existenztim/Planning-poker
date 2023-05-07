92abc28c40448dfb9397033d979d9d5e7e4416dc
dough <viktor.melin@me.com>
2023-05-07 14:59:14 +0200

    move userList socket out from function

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,8 +1,32 @@
 import { io } from 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js';
+import type { User } from './models/userModel';
 
-const URL = "http://localhost:5050";
+const URL = 'http://localhost:5050';
 export const socket = io(URL);
 
 // socket.onAny((event, ...args) => {
 //   console.log(event, args);
 // });
+
+socket.on('userList', (userList: User[]) => {
+  console.log('Receiving update on users in session from server', userList);
+  const votingCardContainer: HTMLDivElement = document.querySelector('.voting-card-container') as HTMLDivElement;
+  votingCardContainer.innerHTML = '';
+
+  userList.map((user) => {
+    console.log(user);
+    const votingCard: HTMLDivElement = document.createElement('div');
+    votingCard.classList.add('voting-card-div');
+    votingCard.innerText = 'Röstkort';
+    votingCard.innerHTML = /*html */ `<p>${user.username}s: poäng</p>
+    <select name="points" id="points">
+      <option value=null>Välj</option>
+      <option value=1>Tiny 1SP</option>
+      <option value=3>Small 3SP</option>
+      <option value=5>Medium 5SP</option>
+      <option value=8>Large 8 SP</option>
+    </select>`;
+
+    votingCardContainer.append(votingCard);
+  });
+});
